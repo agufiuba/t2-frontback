20adc5da1077075f1549e660f65695f4991da912
agu <agufiuba@gmail.com>
2017-10-08 20:13:05 -0300

    Implementa template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { CreateFormComponent } from './create-form/create-form.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    TopbarComponent,
+    CreateFormComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
